5309b159c842d71844a109436c4cee95de4c4f50
Flyquiz <Martiannickel@icloud.com>
2024-02-19 15:17:54 +0300

    Preparing UI for mainVC

--- FortuneTellingByMemes/ViewController.swift
+++ FortuneTellingByMemes/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private lazy var questionTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Введите свой вопрос здесь"
+//        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 5
+        textField.backgroundColor = .white
+        textField.textAlignment = .center
+        
+        return textField
+    }()
+    
+    private lazy var fortuneTellingButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Получить предсказание", for: .normal)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLayout()
     }
-
-
+    
+    
+    private func setupLayout() {
+        self.title = "Fortune Telling By Memes"
+        view.backgroundColor = .systemGray6
+        
+        [questionTextField,
+         fortuneTellingButton].forEach { view.addSubview($0) }
+        
+        let screenSize: CGSize = UIScreen.main.bounds.size
+        NSLayoutConstraint.activate([
+            questionTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            questionTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            questionTextField.widthAnchor.constraint(equalToConstant: screenSize.width - (screenSize.width / 8) * 2),
+            questionTextField.heightAnchor.constraint(equalToConstant: questionTextField.intrinsicContentSize.height * 2),
+            
+            fortuneTellingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            fortuneTellingButton.topAnchor.constraint(equalTo: questionTextField.bottomAnchor, constant: 30),
+            fortuneTellingButton.widthAnchor.constraint(equalTo: questionTextField.widthAnchor)
+        ])
+    }
+    
 }
 
